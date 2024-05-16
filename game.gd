4a3f37d9993b991ed6814079b3612c7791daca8e
Torben Virtmann <Torben.Virtmann@sva.de>
2024-05-16 10:01:58 +0200

    cool scene

--- game.gd
+++ game.gd
@@ -3,15 +3,19 @@ extends ColorRect
 @onready var rayMarchScreen = $"."
 @onready var mat = rayMarchScreen.material as ShaderMaterial
 
-var cameraPosition = Vector3(0.0,0.0,-5.0);
-var cameraAngle = 0.0;
+var cameraPosition = Vector3(0.0,1.0,-5.0);
+var cameraRotation =  Vector3(0.0,0.0,0.0);
 
 var cameraVelocity = Vector3(0.0,0.0,0.0);
-var cameraAngleVelocity = 0.0;
+var cameraRotationVelocity = Vector3(0.0,0.0,0.0);
+
+var fieldOfView = 0.0;
+var fovMax = 10.0;
 
 var drag = 0.9;
 var turnSpeed = 0.3;
-var movementSpeed = 0.5;
+var walkSpeed = 0.5;
+var runSpeed = 2.0;
 
 var RAD = deg_to_rad(90.0);
 
@@ -21,31 +25,65 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	mat.set("shader_parameter/cameraAngle", cameraAngle);
-	mat.set("shader_parameter/cameraPosition", cameraPosition);
-	
+	var movementSpeed;
+	if Input.is_action_pressed("Run"):
+		movementSpeed = runSpeed;
+	else:
+		movementSpeed = walkSpeed;
+	# Camera Position
 	if Input.is_action_pressed("Forward"):
-		cameraVelocity.x += movementSpeed*sin(cameraAngle)*delta;
-		cameraVelocity.z += movementSpeed*cos(cameraAngle)*delta;
+		cameraVelocity.x += movementSpeed*sin(cameraRotation.x)*delta;
+		cameraVelocity.z += movementSpeed*cos(cameraRotation.x)*delta;
 	if Input.is_action_pressed("Backward"):
-		cameraVelocity.x -= movementSpeed*sin(cameraAngle)*delta;
-		cameraVelocity.z -= movementSpeed*cos(cameraAngle)*delta;
+		cameraVelocity.x -= movementSpeed*sin(cameraRotation.x)*delta;
+		cameraVelocity.z -= movementSpeed*cos(cameraRotation.x)*delta;
 	if Input.is_action_pressed("Left"):
-		cameraVelocity.x += movementSpeed*sin(cameraAngle-RAD)*delta;
-		cameraVelocity.z += movementSpeed*cos(cameraAngle-RAD)*delta;
+		cameraVelocity.x += movementSpeed*sin(cameraRotation.x-RAD)*delta;
+		cameraVelocity.z += movementSpeed*cos(cameraRotation.x-RAD)*delta;
 	if Input.is_action_pressed("Right"):
-		cameraVelocity.x += movementSpeed*sin(cameraAngle+RAD)*delta;
-		cameraVelocity.z += movementSpeed*cos(cameraAngle+RAD)*delta;
+		cameraVelocity.x += movementSpeed*sin(cameraRotation.x+RAD)*delta;
+		cameraVelocity.z += movementSpeed*cos(cameraRotation.x+RAD)*delta;
 	if Input.is_action_pressed("Up"):
 		cameraVelocity.y += movementSpeed*delta;
 	if Input.is_action_pressed("Down"):
 		cameraVelocity.y -= movementSpeed*delta;
-	if Input.is_action_pressed("TurnLeft"):
-		cameraAngleVelocity -= turnSpeed*delta;
-	if Input.is_action_pressed("TurnRight"):
-		cameraAngleVelocity += turnSpeed*delta;
-		
-	cameraAngleVelocity *= drag;
+	
+	# Viewing Direction
+	if Input.is_action_pressed("LookUp"):
+		cameraRotationVelocity.y -= turnSpeed*delta;
+	if Input.is_action_pressed("LookDown"):
+		cameraRotationVelocity.y += turnSpeed*delta;
+	if Input.is_action_pressed("LookLeft"):
+		cameraRotationVelocity.x -= turnSpeed*delta;
+	if Input.is_action_pressed("LookRight"):
+		cameraRotationVelocity.x += turnSpeed*delta;
+	
+	# Camera Zoom
+	if Input.is_action_just_released("ZoomIn"):
+		fieldOfView += 10.0*delta;
+	if Input.is_action_just_released("ZoomOut"):
+		fieldOfView -= 10.0*delta;
+	
+	# Apply Drag
 	cameraVelocity *= drag;
+	cameraRotationVelocity *= drag;
+	
+	# Add velocity onto current info
 	cameraPosition += cameraVelocity;
-	cameraAngle += cameraAngleVelocity;
+	cameraRotation += cameraRotationVelocity;
+	
+	# Limit
+	if (cameraRotation.y > RAD):
+		cameraRotation.y = RAD;
+	if (cameraRotation.y < -RAD):
+		cameraRotation.y = -RAD;
+	
+	if (fieldOfView < 0.0):
+		fieldOfView = 0.0;
+	if (fieldOfView > fovMax):
+		fieldOfView = fovMax;
+	
+	# Apply to shader
+	mat.set("shader_parameter/cameraPosition", cameraPosition);
+	mat.set("shader_parameter/cameraRotation", cameraRotation);
+	mat.set("shader_parameter/fieldOfView", fieldOfView);
